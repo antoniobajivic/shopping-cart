dbc66995a28d1f59900916e028d804c6dbb18765
antoniobajivic <bajivicantonio1@gmail.com>
2020-09-08 17:30:38 +0200

    feat: add cart function

--- pages/index.vue
+++ pages/index.vue
@@ -18,16 +18,24 @@
           ></v-img>
 
           <v-card-text class="text--primary">
-            <span>{{product.name}}</span>
-            <span>{{product.price.amount}}</span>
-            <span>{{product.price.measureUnit}} / {{product.price.currency}}</span>
+            <div style="text-align: center">
+              <span class="font-weight-bold">{{product.name}}</span>
+              <br />
+              <span>{{product.price.amount}}</span>
+              <span>{{product.price.measureUnit}} / {{product.price.currency}}</span>
+            </div>
           </v-card-text>
 
           <v-card-actions>
-            <v-btn color="orange" text>Dodaj</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn class="white--text" color="green" shaped depressed @click="addToCart(product)">Dodaj</v-btn>
+            <v-spacer></v-spacer>
           </v-card-actions>
         </v-card>
       </v-col>
+      <div v-for="item in cart" :key="item.id">
+        <span>{{item}}</span>
+      </div>
     </v-row>
   </v-container>
 </template>
@@ -35,15 +43,34 @@
 
 <script>
 import products from "../static/products.json";
+import cloneDeep from "lodash/cloneDeep"
 export default {
   data() {
     return {
       products: products,
+      cart: []
     };
   },
 
   methods: {
-    test() {},
+    addToCart(product) {
+      let hasItem = false;
+      this.cart.forEach(item => {
+        if(item.id === product.id){
+          hasItem = true
+        }
+      });
+      if(hasItem){
+        let index = this.cart.map(function(e) { return e.id; }).indexOf(product.id);
+        this.cart[index].quantity += 1
+        this.cart[index].price.amount += product.price.amount
+      }
+      else{
+        product.quantity = 1
+        let productClone = cloneDeep(product)
+        this.cart.push(productClone)
+      }
+    },
   },
 };
 </script>
